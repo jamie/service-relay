8c0f38b64606bdba9a4bd9bfa790f5ca0049089d
Jamie Macey <jamie.macey@versapay.com>
2012-04-10 08:35:17 -0700

    log PT pings in-memory

--- relay.rb
+++ relay.rb
@@ -1,10 +1,15 @@
 require 'rubygems'
-require 'bundler/now'
+require 'bundler/setup'
 
 require 'sinatra'
 
+$pings = []
+
 get '/' do
-  "Sinatra up and running"
+  "Sinatra up and running with #{$pings.size} pings"
 end
 
+post '/ping' do
+  $pings << params
+end
 
