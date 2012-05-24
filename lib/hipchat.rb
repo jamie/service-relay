07d37b2775c5179844aa83b81b808d481d797a16
Jamie Macey <jamie.macey@versapay.com>
2012-05-24 11:12:04 -0700

    hipchat source file has utf8 inside

--- lib/hipchat.rb
+++ lib/hipchat.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Hipchat
   # http://daringfireball.net/2010/07/improved_regex_for_matching_urls
   URL_REGEX = %r{(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))}
